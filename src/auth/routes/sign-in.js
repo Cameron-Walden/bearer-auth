b871cced1bd51da29a3a9a8a526975d9d6b2bc3b
Cameron <cameronkwalden@gmail.com>
2021-10-26 16:05:50 -0700

    adds starter code with some tweaks

---
+++ src/auth/routes/sign-in.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const { users } = require('./models/index.js');
+const basicAuth = require('./middleware/basic.js')
+const bearerAuth = require('./middleware/bearer.js')
