2610b64736bf7a8c044b24446941878d342da1f3
Cameron <cameronkwalden@gmail.com>
2021-10-26 16:35:38 -0700

    deletes for now

--- src/auth/routes/signup.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const express = require('express');
-const authRouter = express.Router();
-
-const { users } = require('./models/index.js');
-const basicAuth = require('./middleware/basic.js')
-const bearerAuth = require('./middleware/bearer.js')
