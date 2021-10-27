671b1963ffc28d42860e51184573878eabbdd105
Cameron <cameronkwalden@gmail.com>
2021-10-26 18:48:17 -0700

    changes to make async func

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -2,19 +2,37 @@
 
 const { users } = require('../models/index.js');
 
-module.exports = async (req, res, next) => {
 
+async function bearerAuth(req, res, next) {
   try {
+        if (!req.headers.authorization) { next('Invalid Login') }
+    
+        const token = req.headers.authorization.split(' ').pop();
+        const validUser = await users.authenticateWithToken(token);
+    
+        req.user = validUser;
+        req.token = validUser.token;
+    
+      } catch (e) {
+        res.status(403).send('Invalid Login');
+      }
+} 
 
-    if (!req.headers.authorization) { next('Invalid Login') }
+// module.exports = async (req, res, next) => {
 
-    const token = req.headers.authorization.split(' ').pop();
-    const validUser = await users.authenticateWithToken(token);
+//   try {
 
-    req.user = validUser;
-    req.token = validUser.token;
+//     if (!req.headers.authorization) { next('Invalid Login') }
 
-  } catch (e) {
-    res.status(403).send('Invalid Login');
-  }
-}
+//     const token = req.headers.authorization.split(' ').pop();
+//     const validUser = await users.authenticateWithToken(token);
+
+//     req.user = validUser;
+//     req.token = validUser.token;
+
+//   } catch (e) {
+//     res.status(403).send('Invalid Login');
+//   }
+// }
+
+module.exports = bearerAuth;
