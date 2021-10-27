2628c6fc3ba42bb0e07c614ccb19589773cf6d61
Cameron <cameronkwalden@gmail.com>
2021-10-27 11:02:52 -0700

    removes commented out code, adds next()

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -2,36 +2,21 @@
 
 const { users } = require('../models/index.js');
 
-// async function bearerAuth(req, res, next) {
-//   try {
-//         if (!req.headers.authorization) { next('Invalid Login') }
-    
-//         const token = req.headers.authorization.split(' ').pop();
-//         const validUser = await users.authenticateToken(token);
-    
-//         req.user = validUser;
-//         req.token = validUser.token;
-    
-//       } catch (e) {
-//         res.status(403).send('Invalid Login');
-//       }
-// } 
-
 module.exports = async (req, res, next) => {
 
   try {
 
-    if (!req.headers.authorization) { next('Invalid Login') }
+    if (!req.headers.authorization) { 
+      next('Invalid Login');
+     }
 
     const token = req.headers.authorization.split(' ').pop();
     const validUser = await users.authenticateToken(token);
 
     req.user = validUser.username;
     req.token = validUser.token;
-
+    next();
   } catch (e) {
     res.status(403).send('Invalid Login');
   }
 }
-
-// module.exports = bearerAuth;
