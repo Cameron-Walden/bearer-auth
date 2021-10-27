f310c60f3cde3cb8b067477af361f6d2ca2595a9
Cameron <cameronkwalden@gmail.com>
2021-10-27 09:49:39 -0700

    removes commented out code

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -5,21 +5,6 @@ const base64 = require('base-64');
 const { users } = require('../models/index.js')
 const SECRET = process.env.SECRET || 'secretstringfortesting';
 
-// async function basicAuth(req, res, next) {
-//   if(!req.headers.authorization) {
-//     return _authError();
-//   }
-//   let basic = req.headers.authorization;
-//   console.log(basic, 'THIS IS BASIC FROM BASIC.JS');
-//   let [username, pass] = base64.decode(basic).split(':');
-//   try{
-//     req.user = await users.authenticateBasic(username, pass);
-//     next();
-//   } catch (error){
-//     res.status(403).send('Invalid login');
-//   }
-// }
-
 module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { return _authError(); }
