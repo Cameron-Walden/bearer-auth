aa0ac3333d3bbd85b5bc26786a463bc15fbf1329
Cameron <cameronkwalden@gmail.com>
2021-10-27 11:00:19 -0700

    removes console logs and changes user to users

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,25 +1,21 @@
 'use strict';
 
 const base64 = require('base-64');
-// const jwt = require('jsonwebtoken');
 const { users } = require('../models/index.js')
-const SECRET = process.env.SECRET || 'secretstringfortesting';
 
 module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { return _authError(); }
 
   let basic = req.headers.authorization.split(' ');
-  console.log(basic, 'THIS IS BASIC FROM BASIC.JS');
   let encodedString = basic.pop();
   let decodedString = base64.decode(encodedString);
   let [username, pass] = decodedString.split(':');
 
   try {
-    req.user = await user.authenticateBasic(username, pass)
+    req.user = await users.authenticateBasic(username, pass)
     next();
   } catch (e) {
     res.status(403).send('Invalid Login');
   }
-
 }
