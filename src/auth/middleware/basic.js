e4f5b6ed7054f33efa724c2ff3bd4e0d20edc111
Cameron <cameronkwalden@gmail.com>
2021-10-27 09:48:13 -0700

    reverts to starter code and fixes base-64 error

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -2,39 +2,39 @@
 
 const base64 = require('base-64');
 // const jwt = require('jsonwebtoken');
-const { user } = require('../models/index.js')
+const { users } = require('../models/index.js')
 const SECRET = process.env.SECRET || 'secretstringfortesting';
 
-async function basicAuth(req, res, next) {
-  if(!req.headers.authorization) {
-    return _authError();
-  }
-  let basic = req.headers.authorization;
-  console.log(basic, 'THIS IS BASIC FROM BASIC.JS');
-  let [username, pass] = base64.decode(basic).split(':');
-  try{
-    req.user = await user.authenticateBasic(username, pass);
-    next();
-  } catch (error){
-    res.status(403).send('Invalid login');
-  }
-}
-
-// module.exports = async (req, res, next) => {
-
-//   if (!req.headers.authorization) { return _authError(); }
-
+// async function basicAuth(req, res, next) {
+//   if(!req.headers.authorization) {
+//     return _authError();
+//   }
 //   let basic = req.headers.authorization;
 //   console.log(basic, 'THIS IS BASIC FROM BASIC.JS');
 //   let [username, pass] = base64.decode(basic).split(':');
-
-//   try {
-//     req.user = await user.authenticateBasic(username, pass)
+//   try{
+//     req.user = await users.authenticateBasic(username, pass);
 //     next();
-//   } catch (e) {
-//     res.status(403).send('Invalid Login');
+//   } catch (error){
+//     res.status(403).send('Invalid login');
 //   }
-
 // }
 
-module.exports = basicAuth;
+module.exports = async (req, res, next) => {
+
+  if (!req.headers.authorization) { return _authError(); }
+
+  let basic = req.headers.authorization.split(' ');
+  console.log(basic, 'THIS IS BASIC FROM BASIC.JS');
+  let encodedString = basic.pop();
+  let decodedString = base64.decode(encodedString);
+  let [username, pass] = decodedString.split(':');
+
+  try {
+    req.user = await user.authenticateBasic(username, pass)
+    next();
+  } catch (e) {
+    res.status(403).send('Invalid Login');
+  }
+
+}
